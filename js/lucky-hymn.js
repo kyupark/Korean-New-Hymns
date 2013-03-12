245e2a43aea60693141c865105091bb8163fa780
Kyu Park <park@kyu.co>
2013-03-12 14:44:14 -0400

    feeling lucky works on mobile now

--- js/lucky-hymn.js
+++ js/lucky-hymn.js
@@ -1,18 +1,15 @@
 function lucky(num) {    
 	var url = 'http://www.google.com/search?btnI&q=새찬송가+';
 	var text = String(num).split('-').join('+').split('&').join('+');
-	url = url + text + "+장";
+	url = url + text + "장";
 	return url;
 }
 
-$(document).ready(function(){
-	$('.btn-primary').click(function() {
-		window.location.href = lucky($('input').val());
+window.onload = function() {
+	textbox = document.getElementById('nums');
+	textbox.focus();
+	document.getElementById('submit').onclick = function() {
+    	window.location.href = lucky(textbox.value);
 		return false;
-	});
-
-	$('.num').click(function() {
-		$('input').val($('input').val() + $(this).text());
-		return false;
-	})
-});
+	}
+};
